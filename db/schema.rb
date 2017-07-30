2e65c9e758733c4ca17a9f911c4f2e03ccf2d692
hajeongkim <tkdhj5@naver.com>
2017-07-30 10:53:07 +0900

    name add after email

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-rai# This file is auto-generated from the current state of the database. Instead
+# This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@ rai# This file is auto-generated from the current state of the database. Instead
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722022756) do
+ActiveRecord::Schema.define(version: 20170730013854) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170722022756) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", null: false
+    t.string "name"
     t.string "crypted_password"
     t.string "salt"
     t.datetime "created_at", null: false
