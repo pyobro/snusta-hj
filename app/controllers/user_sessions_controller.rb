ccee121bb8ed6068e28a6e03b2e571adfc1bfc32
probro <dev@pellue.com>
2017-08-05 13:12:17 +0900

    initial commit

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,11 +1,11 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, except: [:destroy]
+  skip_before_action :require_login, except: [:destroy, :create]
   def new
     @user = User.new
   end
 
   def create
-    if @user = login(params[:email], params[:password])
+    if @user = login(params[:name], params[:password])
       redirect_back_or_to :users, notice: '로그인 성공'
     else
       flash.now[:alert] = '로그인 실패'
