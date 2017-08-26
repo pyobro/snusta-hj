4aa6f0881b0e1ae1c12b0341b611a3b82451154f
pyobro <pyobro@pellue.com>
2017-08-26 13:20:33 +0900

    bug fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,16 +9,16 @@ class Post < ApplicationRecord
 
   def like_toggle(user)
     if has_like(user)
-      Like.where(post: self, user: user).destroy_all
+      ::Like.where(post: self, user: user).destroy_all
     else
-      Like.create(post: self, user: user)
+      ::Like.create(post: self, user: user)
     end
   end
 
   has_many :comments
 
   def add_comment(user, contents)
-    Comment.create(post: self, user: user, contents: contents)
+    ::Comment.create(post: self, user: user, contents: contents)
   end
 
 end
